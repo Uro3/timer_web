08785105567c98bda22cb7e83e348f3c4ebf2c48
Uro3 <miyakawa.uro3@gmail.com>
2019-02-12 01:39:27 +0900

    modify timer to be able to handle callback

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,8 @@ import './style.scss';
 import Timer from './js/timer';
 
 const timer = new Timer(4000, 500);
+const callback = (time: number) => {
+  console.log(time);
+};
+timer.registerCallback(callback);
 timer.start();
--- src/js/timer.ts
+++ src/js/timer.ts
@@ -3,31 +3,38 @@ class Timer {
   private frequency: number;
   private startTime: number = 0;
   private intervalId: number = 0;
-  private _leftTime: number = 0
+  private callback: (time: number) => void = (time: number) => {};
 
   constructor(targetTime: number, frequency: number) {
     this.targetTime = targetTime;
     this.frequency = frequency;
   }
 
-  get leftTime(): number {
-    return this._leftTime;
-  } 
+  public registerCallback = (callback: (time: number) => void) => {
+    this.callback = callback;
+  }
 
   public start = () => {
     this.startTime = Date.now();
-    this.intervalId = setInterval(this.countdown, this.frequency);
+    const handler = () => {
+      const time = this.countdown();
+      this.callback(time);
+    }
+    this.intervalId = setInterval(handler, this.frequency);
   }
 
   private countdown = () => {
     const pastTime = Date.now() - this.startTime;
-    this._leftTime = this.targetTime - pastTime;
-    console.log(this._leftTime);
-    if(this._leftTime < 0) {
+    const leftTime = this.targetTime - pastTime;
+    if(leftTime < 0) {
       this.stop();
+      return 0;
+    } else {
+      return leftTime;
     }
+
   }
-  
+
   public stop = () => {
     clearInterval(this.intervalId);
   }
